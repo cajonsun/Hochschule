970a32ac02b87f445c04d27e467e136959259941
Ole <Ole@DESKTOP-HQUJHD1.fritz.box>
2018-11-26 18:13:55 +0100

    try and catch

--- Universität/src/Abgabe3_1/GeradeZahl.java
+++ Universität/src/Abgabe3_1/GeradeZahl.java
@@ -1,26 +1,42 @@
 package Abgabe3_1;
 
+
 public class GeradeZahl {
+	int zahl;
 	int grenzeMin = Integer.MIN_VALUE;
 	int grenzeMax = Integer.MAX_VALUE;	
-	int zahl;
 	
-	try {		
-		public GeradeZahl(int zahl) {
-			this.zahl = zahl;
+	
+	public GeradeZahl(int zahl) throws Exception {
+//		try {
+			if (zahl%2 == 0) {
+				this.zahl = zahl;
+			} else {
+				throw new Exception();
+			}
+/*		}
+		catch(Exception e) {
+			System.out.println(e.getMessage());
 		}
+*/			
 	}
 	
-	public GeradeZahl add(GeradeZahl gz2) {
-		int erg = zahl+gz2.zahl;
+	public GeradeZahl add(GeradeZahl gz2) throws Exception {
+		int erg = zahl+gz2.zahl;		
 		GeradeZahl ergebnis = new GeradeZahl(erg);
 		return ergebnis;
 	}
 	
 	public GeradeZahl mult(GeradeZahl gz2) {
-		GeradeZahl ergebnis = new GeradeZahl(zahl*gz2.zahl);
+		GeradeZahl ergebnis = null;
+		try {
+			ergebnis = new GeradeZahl(zahl*gz2.zahl);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return ergebnis;
 	}
-	
-	
+
+
 }
--- Universität/src/Abgabe3_1/GeradeZahlMain.java
+++ Universität/src/Abgabe3_1/GeradeZahlMain.java
@@ -3,13 +3,20 @@ package Abgabe3_1;
 public class GeradeZahlMain {
 
 	public static void main(String[] args) {
-		GeradeZahl gz1 = new GeradeZahl(4);
-		GeradeZahl gz2 = new GeradeZahl(2);
+		try {
+			GeradeZahl gz1 = new GeradeZahl(1);
+			GeradeZahl gz2 = new GeradeZahl(2);
+			
+			GeradeZahl gz3 = gz1.add(gz2);
+			System.out.println("Das Ergebnis der Addition ist "+gz3.zahl);
 		
-		GeradeZahl gz3 = gz1.add(gz2);
-		System.out.println("Das Ergebnis der Addition ist "+gz3.zahl);
+			GeradeZahl gz4 = gz1.mult(gz2);
+			System.out.println("Das Ergebnis der Multiplikation ist "+gz4.zahl);
+		}
+		catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
 		
-		GeradeZahl gz4 = gz1.mult(gz2);
-		System.out.println("Das Ergebnis der Multiplikation ist "+gz4.zahl);
+
 	}
 }
