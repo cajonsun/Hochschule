a4ea7191c407edf8d87a654cdfc44448dfec9ff8
oleja <oleja@Primetab>
2018-11-27 19:33:04 +0100

    änderung Scanner exception

--- Universität/src/Abgabe3_Viereck/ScannerViereck.java
+++ Universität/src/Abgabe3_Viereck/ScannerViereck.java
@@ -19,21 +19,25 @@ public class ScannerViereck {
 			z3 = s.nextInt();
 			System.out.print("Zahl4: ");
 			z4 = s.nextInt();
-
-			if (txt.equals("v")) {
-				name = "Viereck";
-				Viereck v = new Viereck(z1,z2,z3,z4,name);
-				return v;
+			if (z1>0 && z2>0 && z3>0 && z4>0) {
+				if (txt.equals("v")) {
+					name = "Viereck";
+					Viereck v = new Viereck(z1,z2,z3,z4,name);
+					return v;
+				}
+				else if (txt.equals("kv")) {
+					name = "Konvexes Viereck";
+					Konvexes_Viereck kv = new Konvexes_Viereck(z1,z2,z3,z4,name);
+					return kv;
+				}
+				else if (txt.equals("t")) {
+					name = "Trapez";
+					Trapez t = new Trapez(z1,z2,z3,z4,name);
+					return t;
+				}
 			}
-			else if (txt.equals("kv")) {
-				name = "Konvexes Viereck";
-				Konvexes_Viereck kv = new Konvexes_Viereck(z1,z2,z3,z4,name);
-				return kv;
-			}
-			else if (txt.equals("t")) {
-				name = "Trapez";
-				Trapez t = new Trapez(z1,z2,z3,z4,name);
-				return t;
+			else {
+				throw new ViereckException(ViereckException.NEGATIV);
 			}
 		}
 		
@@ -44,15 +48,20 @@ public class ScannerViereck {
 			System.out.print("Zahl2: ");
 			z2 = s.nextInt();
 			
-			if (txt.equals("p")) {
-				name = "Parallelogramm";
-				Parallelogramm p = new Parallelogramm(z1,z2,name);
-				return p;
-			}
-			else if (txt.equals("re")) {
-				name = "Rechteck";
-				Rechteck re = new Rechteck(z1,z2,name);
-				return re;
+			if (z1>0 && z2>0) {
+				if (txt.equals("p")) {
+					name = "Parallelogramm";
+					Parallelogramm p = new Parallelogramm(z1,z2,name);
+					return p;
+				}
+				else if (txt.equals("re")) {
+					name = "Rechteck";
+					Rechteck re = new Rechteck(z1,z2,name);
+					return re;
+				}
+			} 
+			else {
+				throw new ViereckException(ViereckException.NEGATIV);
 			}
 		}
 		
@@ -61,16 +70,20 @@ public class ScannerViereck {
 			System.out.println("Geben Sie 1 Zahlen ein");
 			System.out.print("Zahl1: ");
 			z1 = s.nextInt();
-			
-			if (txt.equals("rh")) {
-				name = "Rhombus";
-				Rhombus p = new Rhombus(z1,name);
-				return p;
+			if (z1>0) {
+				if (txt.equals("rh")) {
+					name = "Rhombus";
+					Rhombus p = new Rhombus(z1,name);
+					return p;
+				}
+				else if (txt.equals("q")) {
+					name = "Quadrat";
+					Quadrat q = new Quadrat(z1,name);
+					return q;
+				}
 			}
-			else if (txt.equals("q")) {
-				name = "Quadrat";
-				Quadrat q = new Quadrat(z1,name);
-				return q;
+			else {
+				throw new ViereckException(ViereckException.NEGATIV);
 			}
 		}
 		return null;
