60268529482998b76b414a8d295d05ac39e2c4c2
Ole <Ole@DESKTOP-HQUJHD1.fritz.box>
2018-12-02 12:04:22 +0100

    Änderung viereck main (Pc)

--- Universität/src/Abgabe3_Viereck/ViereckMain.java
+++ Universität/src/Abgabe3_Viereck/ViereckMain.java
@@ -11,27 +11,13 @@ public class ViereckMain {
 		
 		try {
 			for (int i = 1; i < 12; i++) {
-				if (i==1) {
-					V = new Viereck(1,2,3,4,"Viereck");
-				}
-				else if (i==2) {
-					V = new Konvexes_Viereck(2,3,4,5,"Konvexes Viereck");
-				}
-				else if (i==3) {
-					V = new Trapez(4,3,4,9,"Trapez");
-				}
-				else if (i==4) {
-					V = new Parallelogramm(7,2,"Parallelogramm");
-				}
-				else if (i==5) {
-					V = new Rechteck(8,5,"Rechteck");
-				}
-				else if (i==6) {
-					V = new Rhombus(2,"Rombus");
-				}
-				else if (i==7) {
-					V = new Quadrat(6,"Quadrat");
-				}
+				if (i==1) {V = new Viereck(1,2,3,4,"Viereck");}
+				else if (i==2) {V = new Konvexes_Viereck(2,3,4,5,"Konvexes Viereck");}
+				else if (i==3) {V = new Trapez(4,3,4,9,"Trapez");}
+				else if (i==4) {V = new Parallelogramm(7,2,"Parallelogramm");}
+				else if (i==5) {V = new Rechteck(8,5,"Rechteck");}
+				else if (i==6) {V = new Rhombus(2,"Rombus");}
+				else if (i==7) {V = new Quadrat(6,"Quadrat");}
 				else {			
 					System.out.println("\nWas für eine Art von Viereck wollen sie erstellen: ");
 					System.out.print("Viereck: v\t"+"Konvexes Viereck: kv\n"+"Trapez: t\t");
