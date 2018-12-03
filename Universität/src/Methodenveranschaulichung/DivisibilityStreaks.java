dd959080248636bd9a974f3d91a63e91465d969e
Ole <Ole@DESKTOP-HQUJHD1.fritz.box>
2018-12-03 17:19:44 +0100

    update

---
+++ Universität/src/Methodenveranschaulichung/DivisibilityStreaks.java
@@ -0,0 +1,37 @@
+package Methodenveranschaulichung;
+
+public class DivisibilityStreaks {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		for (int i = 1; i < 32; i++) {
+			sum = sum+pStreak(i,Math.pow(4, i));
+		}
+		System.out.println(sum);
+	}
+	
+	public static int streak(int n) {
+		int i = 1;
+		while (true) {
+			if (n%i==0) {
+				n++;
+				i++;
+			} else {
+				i--;
+				return i;
+			}
+		}
+	}
+	
+	public static int pStreak(int s, double N) {
+		int zwerg;
+		int erg = 0;
+		for (int i = 2; i < N ; i++) {
+			zwerg = streak(i);
+			if (zwerg==s) {
+				erg++;
+			}
+		}
+		return erg;
+	}
+}
