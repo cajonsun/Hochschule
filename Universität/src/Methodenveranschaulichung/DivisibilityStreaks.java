0d0bd85c7efe4330e2e52053e01bd65399174ff5
Ole <Ole@DESKTOP-HQUJHD1.fritz.box>
2018-12-07 15:11:56 +0100

    neu

--- Universität/src/Methodenveranschaulichung/DivisibilityStreaks.java
+++ Universität/src/Methodenveranschaulichung/DivisibilityStreaks.java
@@ -4,13 +4,14 @@ public class DivisibilityStreaks {
 
 	public static void main(String[] args) {
 		int sum = 0;
-		for (int i = 1; i < 32; i++) {
+		for (int i = 31; i < 32; i++) {
+			System.out.println(i);
 			sum = sum+pStreak(i,Math.pow(4, i));
 		}
 		System.out.println(sum);
 	}
 	
-	public static int streak(int n) {
+	public static int streak(double n) {
 		int i = 1;
 		while (true) {
 			if (n%i==0) {
@@ -26,7 +27,7 @@ public class DivisibilityStreaks {
 	public static int pStreak(int s, double N) {
 		int zwerg;
 		int erg = 0;
-		for (int i = 2; i < N ; i++) {
+		for (double i = 2; i < N ; i++) {
 			zwerg = streak(i);
 			if (zwerg==s) {
 				erg++;
