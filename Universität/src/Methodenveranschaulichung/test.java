7f7c22c934bab4d41e122c8ea020c2d218fe6ee4
oleja <oleja@Primetab.fritz.box>
2018-12-15 15:26:05 +0100

    update

---
+++ Universität/src/Methodenveranschaulichung/test.java
@@ -0,0 +1,27 @@
+package Methodenveranschaulichung;
+
+public class test {
+ int a = 1;
+
+public int rueck2(int a) {
+	a = a+1;
+	int c = 7;
+	return c;
+}
+
+public static int rueck() {
+	a = a+1;
+	int c = 9;
+	return c;
+}
+
+public static void main(String[] args) {
+	test Test = new test();
+	System.out.println("a: "+a);
+	int b = Test.rueck2(Test.a);
+	System.out.println("a: "+a+" b: "+b);
+	
+	int c = rueck();
+	System.out.println("a: "+a+" b: "+c);
+}
+}
